60ef19a33746fa3f530d3ef7e21530b7928c17a6
Roberto <robertotupinamba@gmail.com>
2019-11-20 22:56:32 -0300

    Alteração na opção de cadastro

--- js/MenuClick.js
+++ js/MenuClick.js
@@ -1,9 +1,10 @@
 let cadp = '<div id="formContent">' + '<form id="CadProd">' +
     '<input type="text" id="TipProd" class="fadeIn second" name="TipProd" placeholder="Tipo do Produto"></input>' +
     '<input type="text" id="MarcaProd" class="fadeIn second" name="MarcaProd" placeholder="Marca"></input>' +
-    '<input type="text" id="ValorProd" class="fadeIn second" name="ValorProd" placeholder="Valor"></input>' +
+    '<input type="text" id="ValorProd" class="fadeIn second" name="ValorProd" placeholder="Valor" onKeyPress="return(moeda(this,' + "'.'" + ',' + "','" + ',event))"></input>' +
+    '<input type="text" id="QTDProd" class="fadeIn second" name="QTDProd" placeholder="Quantidade"></input>' +
     '<select class="fadeIn third" name="select">' +
-    '<option value="null" disabled selected>Selecione um tamanho</option>'+
+    '<option value="null" disabled selected>Selecione um tamanho</option>' +
     '<option value="PPP">PPP</option> ' +
     '<option value="PP">PP</option>' +
     '<option value="P">P</option>' +
@@ -15,14 +16,21 @@ let cadp = '<div id="formContent">' + '<form id="CadProd">' +
     '<option value="GGG">GGG</option>' +
     '</select>' +
     '<input type="submit" class="fadeIn fourth" value="Cadastar">' +
-
     '</form>' +
     '</div>';
 
+
+
 let cadf = '<div id="formContent">' + '<form id="CadFunc">' +
-    '<input type="text" id="cpf" class="fadeIn second" name="cpf" placeholder="Cpf" maxlength="11"></input>' +
-    '<input type="text" id="nome" class="fadeIn third" name="nome" placeholder="Nome" maxlength="100">' +
-    '<input type="text" id="password" class="fadeIn third" name="senha" placeholder="Senha" maxlength="32">' +
+    '<input type="text" id="cpfFunc" class="fadeIn second" name="cpfFunc" placeholder="Cpf" maxlength="11"></input>' +
+    '<input type="text" id="nomeFunc" class="fadeIn third" name="nomeFunc" placeholder="Nome" maxlength="100">' +
+    '<input type="text" id="TelFunc" class="fadeIn third" name="TelFunc" placeholder="Telefone" required >' +
+    '<input type="text" id="ruaFunc" class="fadeIn third" name="ruaFunc" placeholder="Rua">' +
+    '<input type="number" id="numFunc" class="fadeIn third" name="numFunc" placeholder="Numero">' +
+    '<input type="text" id="bairroFunc" class="fadeIn third" name="bairroFunc" placeholder="Bairro">' +
+    '<input type="text" id="cidadeFunc" class="fadeIn third" name="cidadeFunc" placeholder="Cidade">' +
+    '<input type="text" id="cepFunc" class="fadeIn third" name="cepFunc" placeholder="CEP">' +
+    '<input type="text" id="passwordFunc" class="fadeIn third" name="senhaFunc" placeholder="Senha" maxlength="32">' +
     '<input type="submit" class="fadeIn fourth" value="Cadastar">' +
     '</form>' +
     '</div>';
@@ -71,6 +79,8 @@ $(document).ready(function () {
         $("#h").empty();
         $("#h").html(cadf);
         $(".masthead-brand").text("Cadastrar Funcionarios");
+        $("input#TelFunc").mask("(99) 99999-999?9")
+        $("input#cepFunc").mask("99999-999")
         //$("#script").html('<script src="./js/request.js"></script>');
     });
     $("#LP").click(function (e) {
