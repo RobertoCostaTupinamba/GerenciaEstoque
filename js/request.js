1233457568ad12041308c4145fadeff2b7fc9fc3
Roberto <robertotupinamba@gmail.com>
2019-11-24 01:00:11 -0300

    request atualizado2

--- js/request.js
+++ js/request.js
@@ -3,6 +3,14 @@ $(function () {
         e.preventDefault();
         var dados_form = $(this).serialize();
         console.log(dados_form);
-        
+        $.ajax({
+            type: "POST",
+            url: "./php/cadastroProdutos.php",
+            data: dados_form,
+            dataType: "JSON",
+            success: function (response) {
+                
+            }
+        });
     });
 });
