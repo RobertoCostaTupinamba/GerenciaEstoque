eed989f2bf8ada1b29dde022a2c0efa93b7c373b
MateusDeFreitasRosa <mateusfreitasrosa@hotmail.com>
2019-12-01 23:43:47 -0300

    FinalizacaoCorrecoesAtualizar

--- php/retornoAtualiza.php
+++ php/retornoAtualiza.php
@@ -1,14 +1,9 @@
 <?php
-    if ($res = pg_send_query($conexao, $select)) {
-        $retorno = pg_get_result($conexao);
+    if ($res = pg_query($conexao, $select)) {
         $arrayRes = pg_fetch_all($res);
         $arrayJson = json_encode( $arrayRes, JSON_UNESCAPED_UNICODE);
         
-        
-        if (if (pg_result_error_field($retorno, PGSQL_DIAG_SOURCE_FUNCTION) == '_bt_check_unique') {
-			echo "PK Repetida";
-		}
-        else if($arrayJson == "false")
+        if($arrayJson == "false")
             echo "1"; // Não existe esse usuario cadastrado.
         else
             echo $arrayJson;
